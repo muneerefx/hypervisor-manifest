a5fb91a65230ad650a3c134f4cb802192bb32a3a
Muneer Moosa <Muneer.Moosa@electrifex.com>
2026-02-20 12:26:55 +0530

    Update filegernartor.sh

--- filegernartor.sh
+++ filegernartor.sh
@@ -2,5 +2,19 @@
 
 for i in {1..5}
 do
-  touch "build-cwo$i"
+  dir="build-cwo$i"
+  mkdir -p "$dir"
+
+  # Create 5 build-cwo files
+  for j in {1..5}
+  do
+    touch "$dir/build-cwo$j"
+  done
+
+  # Create 5 linuximage random files
+  for k in {1..5}
+  do
+    rand=$(printf "%07d" $((RANDOM%9000000+1000000)))
+    touch "$dir/linuximage-$rand.wic"
+  done
 done
