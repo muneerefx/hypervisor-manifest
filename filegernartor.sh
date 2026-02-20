54751c79a5c0cbcfd0b9a414f704e555fc08bec0
Muneer Moosa <Muneer.Moosa@electrifex.com>
2026-02-20 14:37:29 +0530

    Update filegernartor.sh

--- filegernartor.sh
+++ filegernartor.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
-# Create build folders
+# Create 5 build-cwo files (NOT directories)
 for i in {1..5}
 do
-  dir="build-cwo$i"
-  mkdir -p "$dir"
-
-  # Create 5 build-cwo files
-  for j in {1..5}
-  do
-    touch "$dir/build-cwo$j"
-  done
-
-  # Create 5 linuximage random files
-  for k in {1..5}
-  do
-    rand=$(printf "%07d" $((RANDOM%9000000+1000000)))
-    touch "$dir/linuximage-$rand.wic"
-  done
+  touch "build-cwo$i"
 done
 
+# Create 5 linuximage random files (in current directory)
+for i in {1..5}
+do
+  rand=$(printf "%07d" $((RANDOM%9000000+1000000)))
+  touch "linuximage-$rand.wic"
+done
 
-# Create out directory
+# Create only ONE directory: out
 mkdir -p out
 
-# Create dummy Android image files
+# Create dummy Android image files inside out/
 touch out/boot.img
 touch out/system.img
 touch out/vendor.img
 touch out/userdata.img
 touch out/recovery.img
 
-echo "Structure created successfully."
+echo "Files and out directory created successfully."
